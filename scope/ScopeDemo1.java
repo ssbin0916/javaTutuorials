569a6ab4a9d8fc958b80253744729f13e5593951
seongbin <151374753+ssbin0916@users.noreply.github.com>
2023-11-23 17:54:35 +0900

    Add files via upload

---
+++ scope/ScopeDemo1.java
@@ -0,0 +1,15 @@
+package scope;
+
+public class ScopeDemo1 {
+
+    static void a() {
+        int i = 0;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            a();
+            System.out.println(i);
+        }
+    }
+}
