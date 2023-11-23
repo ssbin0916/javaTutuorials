c8f9a24e1c8edd7454dd3be07d354481174182fd
seongbin <151374753+ssbin0916@users.noreply.github.com>
2023-11-23 17:45:45 +0900

    Add files via upload

---
+++ inheritance/CalculatorDemo3.java
@@ -0,0 +1,19 @@
+package inheritance;
+
+class DivisionableCalcuator extends MultiplicationableCalculator {
+    public void division() {
+        System.out.println(this.left / this.right);
+    }
+}
+
+public class CalculatorDemo3 {
+    public static void main(String[] args) {
+
+        DivisionableCalcuator c1 = new DivisionableCalcuator();
+        c1.setOrands(10, 20);
+        c1.sum();
+        c1.avg();
+        c1.multiplication();
+        c1.division();
+    }
+}
