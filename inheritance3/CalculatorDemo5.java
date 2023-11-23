42a15cc610949d38bdc99af6906519d48800a784
seongbin <151374753+ssbin0916@users.noreply.github.com>
2023-11-23 17:46:56 +0900

    Add files via upload

---
+++ inheritance3/CalculatorDemo5.java
@@ -0,0 +1,47 @@
+package inheritance3;
+
+class Calculator {
+    int left, right;
+
+    public Calculator() {
+    }
+
+    public Calculator(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public void setOrands(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public void sum() {
+        System.out.println(this.left + this.right);
+    }
+
+    public void avg() {
+        System.out.println((this.left + this.right) / 2);
+    }
+}
+
+class SubstractionableCalculator extends Calculator {
+    public SubstractionableCalculator(int left, int right) {
+        //this.left = left;
+        //this.right = right;
+        super(left, right);
+    }
+
+    public void substract() {
+        System.out.println(this.left - this.right);
+    }
+}
+
+public class CalculatorDemo5 {
+    public static void main(String[] args) {
+        SubstractionableCalculator c1 = new SubstractionableCalculator(10, 20);
+        c1.sum();
+        c1.avg();
+        c1.substract();
+    }
+}
