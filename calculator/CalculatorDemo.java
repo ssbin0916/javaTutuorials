57bb2687d7619a3a43fad7535128b09e84384875
seongbin <151374753+ssbin0916@users.noreply.github.com>
2023-11-23 17:49:17 +0900

    Add files via upload

---
+++ calculator/CalculatorDemo.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class CalculatorDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println(10 + 20);
+        System.out.println(20 + 40);
+    }
+
+}
