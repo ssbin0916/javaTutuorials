98c40cfa65c8960429849ecff8428ab45ee22333
seongbin <151374753+ssbin0916@users.noreply.github.com>
2023-12-01 23:25:42 +0900

    Add files via upload

---
+++ condition/LoginDemo.java
@@ -0,0 +1,18 @@
+package condition;
+
+public class LoginDemo {
+    public static void main(String[] args) {
+        String id = args[0];
+        String password = args[1];
+        if (id.equals("egoing")) {
+            if (password.equals("111111")) {
+                System.out.println("right");
+            } else {
+                System.out.println("wrong");
+            }
+
+        } else {
+            System.out.println("wrong");
+        }
+    }
+}
---
+++ condition/SwitchDemo.java
@@ -0,0 +1,57 @@
+package condition;
+
+public class SwitchDemo {
+    public static void main(String[] args) {
+
+        System.out.println("switch(1)");
+        switch (1) {
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+        }
+
+        System.out.println("switch(2)");
+        switch (2) {
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+        }
+
+        System.out.println("switch(3)");
+        switch (3) {
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+        }
+
+        System.out.println("switch(4)");
+        switch (4) {
+            case 1:
+                System.out.println("one");
+            case 2:
+                System.out.println("two");
+            case 3:
+                System.out.println("three");
+            case 4:
+                System.out.println("default");
+        }
+    }
+}
