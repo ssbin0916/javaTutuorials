2712265a1c994d1f88b8cb1d702a523f9572c951
seongbin <151374753+ssbin0916@users.noreply.github.com>
2023-12-02 19:15:35 +0900

    Add files via upload

---
+++ coditionaloperator/NotDemo.java
@@ -0,0 +1,15 @@
+package coditionaloperator;
+
+public class NotDemo {
+
+    public static void main(String[] args) {
+
+        if (!true) {
+            System.out.println(1);
+        }
+
+        if (!false) {
+            System.out.println(2);
+        }
+    }
+}
---
+++ coditionaloperator/OrDemo.java
@@ -0,0 +1,23 @@
+package coditionaloperator;
+
+public class OrDemo {
+
+    public static void main(String[] args) {
+
+        if (true || true) {
+            System.out.println(1);
+        }
+
+        if (true || false) {
+            System.out.println(2);
+        }
+
+        if (false || true) {
+            System.out.println(3);
+        }
+
+        if (false || false) {
+            System.out.println(4);
+        }
+    }
+}
