93f4b3fa0f98de7df61a4552458738afd2255a03
seongbin <151374753+ssbin0916@users.noreply.github.com>
2023-12-03 12:21:59 +0900

    Add files via upload

---
+++ loop/WhileDemo.java
@@ -0,0 +1,13 @@
+package loop;
+
+public class WhileDemo {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+        while (i < 10) {
+            System.out.println("Coding Everybody" + i);
+            i++;
+        }
+    }
+}
